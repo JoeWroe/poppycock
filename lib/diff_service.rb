5af83590eecc90e5bf071428dd487ccb86ef2099
JoeWroe <joe_wroe@icloud.com>
2020-05-25 13:00:40 +0100

    Set the complete diff and make it accessible
    JW

--- lib/diff_service.rb
+++ lib/diff_service.rb
@@ -1,10 +1,13 @@
 class DiffService
 
+  attr_reader :complete_diff
+
   def initialize(command_executor:)
     @command_executor = command_executor
+    @complete_diff = ""
   end
 
   def get_diff_for_unstaged_files
-    complete_diff = @command_executor.execute_git_command("diff")
+    @complete_diff = @command_executor.execute_git_command("diff")
   end
 end
--- spec/diff_service_spec.rb
+++ spec/diff_service_spec.rb
@@ -6,11 +6,12 @@ describe DiffService do
     
     it "should obtain the complete diff" do
       command_executor = double("command_executor")
-      diff_service = DiffService.new(command_executor: command_executor)
-
       allow(command_executor).to receive(:execute_git_command).and_return("a git diff")
-      
-      expect(diff_service.get_diff_for_unstaged_files()).to eq("a git diff")
+
+      diff_service = DiffService.new(command_executor: command_executor)
+      diff_service.get_diff_for_unstaged_files()
+
+      expect(diff_service.complete_diff).to eq("a git diff")
     end
   end
 end
